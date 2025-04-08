3ab5554361375a1c99ba823df0e6f0b98b0b82f0
Smittysource <gisshan8@gmail.com>
2025-04-07 22:03:14 -0400

    decreased resources

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main ():
     pygame.init()
     screen = pygame.display.set_mode(((SCREEN_WIDTH, SCREEN_HEIGHT)))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,7 @@ def main ():
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__":
     main()
